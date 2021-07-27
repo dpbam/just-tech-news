855b5d1f8ec0000de160d8d86c6a1bc8d1b8b2a8
Derek Phelps <derekphtpt@gmail.com>
2021-07-27 10:28:24 -0600

    server not hearing

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// GET /api/users
+// GET /api/users, all users
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method)
     User.findAll({
