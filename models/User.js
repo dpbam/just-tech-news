2b9af9e60c1431359b768469c1b95f84364e7a36
Derek Phelps <derekphtpt@gmail.com>
2021-07-27 13:44:18 -0600

    dbUserData.checkPassword is not a function?

--- models/User.js
+++ models/User.js
@@ -73,6 +73,13 @@ User.init(
         underscored: true,
         // make it so our model name stays lowercase in the database
         modelName: 'user'
+    },
+    // create our User model
+    class User extends Model {
+        // set up method to run on instance data (per user) to check password
+        checkPassword(loginPw) {
+            return bcrypt.compareSync(loginPw, this.password);
+        }
     }
 );
 
