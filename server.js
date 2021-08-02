cee00284ce46c7b6ac0b4909593dcde19fb1a625
Derek Phelps <derekphtpt@gmail.com>
2021-08-02 14:51:15 -0600

    still not working homepage

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
+const routes = require('./controllers/');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3301;
 
 const sequelize = require('./config/connection');
 
-
-const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
@@ -16,13 +15,18 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// turn on routes
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static('views'));
 
-// turn on routes
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+// sequelize.sync({ force: false }).then(() => {
+//     app.listen(PORT, () => console.log('Now listening'));
+// });
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+    sequelize.sync({ force: false });
 });
