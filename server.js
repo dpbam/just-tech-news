1fd90e4f5b41b39f052677927d913fcc024af1d8
Derek Phelps <derekphtpt@gmail.com>
2021-08-02 14:18:02 -0600

    homepage not showing up

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
 const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3301;
 
+const sequelize = require('./config/connection');
+
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static('views'));
 
 // turn on routes
 app.use(routes);
