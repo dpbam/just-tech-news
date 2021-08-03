9f3a451345d17f6ab764f36ab6755fdbc18a4d71
Derek Phelps <derekphtpt@gmail.com>
2021-08-03 14:45:26 -0600

    14.2.5 attempt to fix vs code

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,11 +63,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+        .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        })
+    })
 });
 
 router.post('/login', (req, res) => {
@@ -84,6 +88,7 @@ router.post('/login', (req, res) => {
 
         // Verify user
         const validPassword = dbUserData.checkPassword(req.body.password);
+        
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect password!' });
             return;
