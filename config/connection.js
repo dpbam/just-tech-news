ee158c994e3d3632dd00d951668a366a08da7b16
Derek Phelps <derekphtpt@gmail.com>
2021-08-03 20:03:20 -0600

    fix exclamation mark on connection.js?

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,12 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+console.log(process.env.JAWSDB_URL)
+console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW)
 
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-// create connection to our database, pass in your MySQL information for username and password
+    sequelize = new Sequelize(proces.env.JAWSDB_URL);
+}   else    {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
--- server.js
+++ server.js
@@ -1,14 +1,39 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+const routes = require('./controllers/');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3301;
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.SECRET_THING,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // turn on routes
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static('views'));
+
 app.use(routes);
 
 // turn on connection to db and server
